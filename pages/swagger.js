bee9c26ca8aba5d8b8238513824658117c8dc885
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-09-09 21:45:46 +0700

    Add files via upload

--- pages/swagger.js
+++ pages/swagger.js
@@ -1,6 +1,5 @@
+// pages/swagger.js
 import dynamic from "next/dynamic";
-import "swagger-ui-react/swagger-ui.css";
-import "../styles/swagger-modern.css";
 
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
