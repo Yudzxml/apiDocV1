b08083e2d337ea31e55348ff1765297a280ec066
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-09-09 22:03:34 +0700

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "swagger-ui-react/swagger-ui.css";
-import "../styles/swagger-modern.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
